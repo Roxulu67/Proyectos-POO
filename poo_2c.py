7645600ff9ac86d321db947a293d003ee5eff0fd
Roxulu67 <rodrigogt03@gmail.com>
2025-01-08 13:23:30 -0600

    Creando atributos de la clase

--- poo_2c.py
+++ poo_2c.py
@@ -1,2 +1,22 @@
-camelCase y snake_case
-HolaEscriboComoCamelCase
+class Personaje:
+    #Atributos de la clase
+    nombre = 'Default'
+    fuerza = 0
+    inteligencia = 0
+    defensa = 0
+    vida = 0
+
+#Variable del constructor vacío
+mi_personaje = Personaje()
+#Modificando valores de los atributos
+mi_personaje.nombre = "Rodrigo"
+mi_personaje.fuerza = 400
+mi_personaje.inteligencia = 500
+mi_personaje.defensa = 1000
+mi_personaje.vida =100
+
+print("El nombre de mi personaje es: ",mi_personaje.nombre)
+print("El nombre de mi personaje es: ",mi_personaje.fuerza)
+print("El nombre de mi personaje es: ",mi_personaje.inteligencia)
+print("El nombre de mi personaje es: ",mi_personaje.defensa)
+print("El nombre de mi personaje es: ",mi_personaje.vida)
